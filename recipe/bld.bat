a9b867cef53332360e363afb6eeac1b0f9dd8a9a
Ray Donnelly <mingw.android@gmail.com>
2018-04-29 22:06:56 +0100

    Updates from upstream numba

---
+++ recipe/bld.bat
@@ -0,0 +1,12 @@
+
+@rem Let CMake know about the LLVM install path, for find_package()
+set CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
+
+@rem Ensure there are no build leftovers (CMake can complain)
+if exist ffi\build rmdir /S /Q ffi\build
+
+python -S setup.py install
+if errorlevel 1 exit 1
+
+python runtests.py
+if errorlevel 1 exit 1
