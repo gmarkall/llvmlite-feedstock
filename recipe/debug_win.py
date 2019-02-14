1e48821f2d2fb8fc23889afd0b55c4679487d893
Pearu Peterson <pearu.peterson@gmail.com>
2019-02-15 00:17:00 +0200

    cleanup

--- recipe/debug_win.py
+++
@@ -1,9 +0,0 @@
-
-import os
-import sys
-from os.path import join
-print(__file__)
-for root, dirs, files in os.walk('C:\\'):
-    for d in dirs:
-        if d in ['MSBuild', 'Microsoft.Cpp']:
-            print('Found ' + join(root, d))
