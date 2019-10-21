001a50e3ea6f7aa153ad9660ec18bcec991b8459
Isuru Fernando <isuruf@gmail.com>
2019-10-21 15:20:21 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,5 +6,9 @@ DARWIN_TARGET=$macos_machine
 
 export PYTHONNOUSERSITE=1
 
+# Enables static linking of stdlibc++
+# TODO: This should be fixed
+export LLVMLITE_CXX_STATIC_LINK=1
+
 $PYTHON setup.py build --force
 $PYTHON setup.py install
