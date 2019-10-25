32200f0a9a4eb1e795f234f68f85fe34821015c4
John Kirkham <jakirkham@gmail.com>
2019-10-24 22:19:41 -0400

    Stop statically linking stdlibc++

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,9 +6,5 @@ DARWIN_TARGET=$macos_machine
 
 export PYTHONNOUSERSITE=1
 
-# Enables static linking of stdlibc++
-# TODO: This should be fixed
-export LLVMLITE_CXX_STATIC_LINK=1
-
 $PYTHON setup.py build --force
 $PYTHON setup.py install
