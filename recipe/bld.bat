122ba9c2ccd2adc0bf571133de37a2ffdb700f7f
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2020-06-24 17:36:10 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,11 @@
 @rem Let CMake know about the LLVM install path, for find_package()
 set CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
 set ZLIB_ROOT_DIR=%LIBRARY_PREFIX%
-set LLVM_CONFIG=%LIBRARY_PREFIX%/bin/llvm_config.exe
+set LLVM_CONFIG=%LIBRARY_PREFIX%\bin\llvm_config.exe
 set CMAKE_GENERATOR=Visual Studio 15 2017 Win64
 set ZLIB_NAME=zlib
+set ZLIB_LIBRARY_PATH=%LIBRARY_LIB%
+set ZLIB_INCLUDE=%LIBRARY_INC%
 
 
 @rem Ensure there are no build leftovers (CMake can complain)
