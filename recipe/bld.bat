2ad27e193f075efdba03c5798bb20841deea1158
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2020-06-25 14:19:14 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -14,6 +14,8 @@ set PREFIX=%LIBRARY_PREFIX%
 @rem Ensure there are no build leftovers (CMake can complain)
 if exist ffi\build rmdir /S /Q ffi\build
 
+llvm-config.exe --libs
+
 %PYTHON% -S setup.py install
 if errorlevel 1 exit 1
 
