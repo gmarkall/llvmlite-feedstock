d76cde2384436a0bcada5eea64f0197315684bc8
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2020-06-26 11:47:21 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -20,6 +20,7 @@ llvm-config.exe --libs
 @rem Trying to invoke cmake directly
 mkdir build
 cd build
+cmake ../ffi/ --verbose
 cmake --build ../ffi/ --config Release --verbose
 if errorlevel 1 exit 1
 
