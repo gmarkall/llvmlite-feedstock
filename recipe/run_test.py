d9cde1b7aa9abe8e557cec11525270df4bbc769a
jakirkham <jakirkham@gmail.com>
2022-08-09 10:07:21 -0700

    Fix typo in comment of `run_test.py`

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -2,6 +2,6 @@ import os
 from llvmlite.tests import main
 
 # Disable tests for distribution only
-# These check for static linkage, which don't do.
+# These check for static linkage, which we don't do.
 os.environ['LLVMLITE_DIST_TEST'] = ''
 main()
